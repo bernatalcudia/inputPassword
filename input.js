8126cb04b0d004db0fe8b640fe661ef683bd19b5
bernat alcudia <bernatalcudia@gmail.com>
2025-02-06 12:52:22 +0100

    add estructure to js and modified styles

---
+++ input.js
@@ -0,0 +1,8 @@
+const inputBox = document.getElementById('input-box');
+const h3 = document.createElement('h3');
+
+function passwordRequired(params) {
+
+}
+
+inputBox.addEventListener('keypress', passwordRequired)
