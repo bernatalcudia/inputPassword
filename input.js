68e30d25558b5d4b051056c846e0ba45f23fec98
bernat alcudia <bernatalcudia@gmail.com>
2025-02-06 13:00:40 +0100

    add ifs to js

--- input.js
+++ input.js
@@ -2,7 +2,15 @@ const inputBox = document.getElementById('input-box');
 const h3 = document.createElement('h3');
 
 function passwordRequired(params) {
+    const lengthPassword = Number(inputBox.value)
+    if (lengthPassword < 10) {
 
+    } if (lengthPassword < 8) {
+
+    } else {
+
+    }
+    h3.innerText = 'The password is not save'
 }
 
-inputBox.addEventListener('keypress', passwordRequired)
+inputBox.addEventListener('keydown', passwordRequired);
