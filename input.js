a16798797b0561b7c0f545b59d775ca28f1feadf
bernat alcudia <bernatalcudia@gmail.com>
2025-02-06 13:41:19 +0100

    fixed code to ifs

--- input.js
+++ input.js
@@ -3,18 +3,20 @@ const h3 = document.createElement('h3');
 
 function passwordRequired() {
     const lengthPassword = inputBox.value.length
-    if (lengthPassword > 10) {
+    if (lengthPassword > 9) {
         h3.style.color = 'green';
-        h3.innerText = 'The password is save'
-    } if (lengthPassword > 8 && lengthPassword == 10) {
+        h3.innerText = 'The password is save';
+    } if (lengthPassword > 7 && lengthPassword < 9) {
         h3.style.color = 'yellow';
-        h3.innerText = 'The password is not save'
-    } if (lengthPassword < 8) {
+        h3.innerText = 'The password is not save';
+    } if (lengthPassword < 7) {
         h3.style.color = 'red';
-        h3.innerText = 'The password is to short'
+        h3.innerText = 'The password is to short';
     }
 
-    document.body.appendChild(h3)
+    console.log(lengthPassword)
+
+    document.body.appendChild(h3);
 }
 
 inputBox.addEventListener('keydown', passwordRequired);
