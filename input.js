b9fac1bf84ebc6e56e5cb56edab8616dc8662757
JNBarrachina <jnb.pasadobinario@gmail.com>
2025-02-07 12:33:03 +0100

    Some fixes

--- input.js
+++ input.js
@@ -1,19 +1,37 @@
-const inputBox = document.getElementById('input-box');
-const h3 = document.createElement('h3');
+const inputBox = document.getElementById("input-box");
+const h3 = document.createElement("h3");
+const inputName = document.getElementById("input-name");
+const login = document.getElementById("butLog");
 
 function passwordRequired() {
-    const lengthPassword = inputBox.value.length
+    const lengthPassword = inputBox.value.length;
+    const lengthName = inputName.value.length;
+
     if (lengthPassword > 9) {
-        h3.style.color = 'green';
-        h3.innerText = 'The password is save';
-    } if (lengthPassword > 7 && lengthPassword < 9) {
-        h3.style.color = 'yellow';
-        h3.innerText = 'The password is not save';
-    } if (lengthPassword < 7) {
-        h3.style.color = 'red';
-        h3.innerText = 'The password is to short';
+        h3.style.color = "green";
+        h3.innerText = "The password is save";
+        login.setAttribute("class", "butLogin");
+        login.removeAttribute("disabled");
+    }
+    if (lengthPassword > 7 && lengthPassword < 9) {
+        h3.style.color = "orange";
+        h3.innerText = "The password is not save";
+        login.removeAttribute("class", "butLogin");
+        login.setAttribute("disabled", "true");
+    }
+    if (lengthPassword < 7) {
+        h3.style.color = "red";
+        h3.innerText = "The password is too short";
+        login.removeAttribute("class", "butLogin");
+        login.setAttribute("disabled", "true");
     }
     document.body.appendChild(h3);
 }
 
-inputBox.addEventListener('keydown', passwordRequired);
+function nameRequired() {
+    if (inputName.value.length == 0) {
+    }
+}
+
+inputBox.addEventListener("keydown", passwordRequired);
+inputName.addEventListener("keydown", nameRequired);
