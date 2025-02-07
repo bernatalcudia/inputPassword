5b3f7693223d870d61a04be3996aa974fab2d3ae
Juan Narros <167420227+JNBarrachina@users.noreply.github.com>
2025-02-07 13:10:29 +0100

    Feature: new validation. (#1)
    * Some fixes
    
    * Feature finished

--- input.js
+++ input.js
@@ -1,19 +1,43 @@
-const inputBox = document.getElementById('input-box');
-const h3 = document.createElement('h3');
+const inputBox = document.getElementById("input-box");
+const h3 = document.createElement("h3");
+const inputName = document.getElementById("input-name");
+const login = document.getElementById("butLog");
 
 function passwordRequired() {
-    const lengthPassword = inputBox.value.length
-    if (lengthPassword > 9) {
-        h3.style.color = 'green';
-        h3.innerText = 'The password is save';
-    } if (lengthPassword > 7 && lengthPassword < 9) {
-        h3.style.color = 'yellow';
-        h3.innerText = 'The password is not save';
-    } if (lengthPassword < 7) {
-        h3.style.color = 'red';
-        h3.innerText = 'The password is to short';
+    const lengthPassword = inputBox.value.length;
+    const lengthName = inputName.value.length;
+
+    if (lengthPassword > 9 && lengthName > 0) {
+        h3.style.color = "green";
+        h3.innerText = "The password is save";
+        login.setAttribute("class", "butLogin");
+        login.removeAttribute("disabled");
+    }
+    if (lengthPassword > 7 && lengthPassword < 9) {
+        h3.style.color = "orange";
+        h3.innerText = "The password is not save";
+        login.removeAttribute("class", "butLogin");
+        login.setAttribute("disabled", "true");
+    }
+    if (lengthPassword < 7) {
+        h3.style.color = "red";
+        h3.innerText = "The password is too short";
+        login.removeAttribute("class", "butLogin");
+        login.setAttribute("disabled", "true");
     }
     document.body.appendChild(h3);
 }
 
-inputBox.addEventListener('keydown', passwordRequired);
+function nameRequired() {
+    if (inputName.value.length == 0) {
+        h3.style.color = "red";
+        h3.innerText = "Introduce your name";
+        login.removeAttribute("class", "butLogin");
+        login.setAttribute("disabled", "true");
+    } else {
+        passwordRequired();
+    }
+}
+
+inputBox.addEventListener("input", passwordRequired);
+inputName.addEventListener("input", nameRequired);
